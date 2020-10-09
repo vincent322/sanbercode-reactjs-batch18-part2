882aef6fed9dd4eda9eb282a057049098b41518f
vincent322 <vincentchrist99@gmail.com>
2020-10-09 20:30:14 +0700

    making components & props

---
+++ projectreactjs/src/Tugas-10/TabelHargaBuah.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+let dataHargaBuah = [
+    {nama: "Semangka", harga: 10000, berat: 1000},
+    {nama: "Anggur", harga: 40000, berat: 500},
+    {nama: "Strawberry", harga: 30000, berat: 400},
+    {nama: "Jeruk", harga: 30000, berat: 1000},
+    {nama: "Mangga", harga: 30000, berat: 500}
+]
+
+class NamaBuah extends React.Component {
+    render() {
+        return (
+            <>
+                <td style={{backgroundColor: 'tomato'}}>{this.props.name}</td>
+            </>
+        );
+    }
+}
+
+class HargaBuah extends React.Component {
+    render() {
+        return (
+            <>
+                <td style={{backgroundColor: 'tomato'}}>{this.props.name}</td>
+            </>
+        );
+    }
+}
+
+class BeratBuah extends React.Component {
+    render() {
+        return (
+            <>
+                <td style={{backgroundColor: 'tomato'}}>{this.props.name/1000} Kg</td>
+            </>
+        );
+    }
+}
+
+function TblHargaBuah() {
+    return (
+        <>
+            <div style={{margin: '50px auto', border: '5px solid #aaa', borderRadius: '10px', width: '75%', padding: '50px', backgroundColor: '#FFF'}}>
+                <h1 style={{textAlign: 'center'}}>Tabel Harga Buah</h1>
+                <table border="1" style={{width: '100%'}}>
+                    <tr>
+                        <th style={{width: '44%', backgroundColor: 'grey'}}>Nama</th>
+                        <th style={{width: '40%', backgroundColor: 'grey'}}>Harga</th>
+                        <th style={{width: '96%', backgroundColor: 'grey'}}>Berat</th>
+                    </tr>
+                    {dataHargaBuah.map(el=> {
+                        return (
+                        <tr>
+                            <NamaBuah name={el.nama}/>
+                            <HargaBuah name={el.harga}/>
+                            <BeratBuah name={el.berat}/>
+                        </tr>
+                        );
+                    })}
+                </table>
+            </div>
+        </>
+    );
+}
+
+export default TblHargaBuah
---
+++ projectreactjs/src/Tugas-9/Buah.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Buah() {
+    return (
+      <div style={{margin: 'auto', border: '5px solid #aaa', borderRadius: '10px', width: '50%', padding: '30px', backgroundColor: '#FFF'}}>
+      <h1 style={{textAlign: 'center'}}>Form Pembelian Buah</h1>
+        <form>
+          <table className="table">
+            <tr>
+              <td width="300" margin="50px auto" padding="10px" align="center"><label className="text-bold">Nama Pelanggan</label></td>
+              <td><input type="text" name="nama"></input></td>
+            </tr>
+            <tr>
+              <td><label className="text-bold">Daftar Item</label></td>
+              <td>
+                <input type="checkbox" name="semangka"></input><label>Semangka</label><br></br>
+                <input type="checkbox" name="jeruk"></input><label>Jeruk</label><br></br>
+                <input type="checkbox" name="nanas"></input><label>Nanas</label><br></br>
+                <input type="checkbox" name="salak"></input><label>Salak</label><br></br>
+                <input type="checkbox" name="anggur"></input><label>Anggur</label>
+              </td>
+            </tr>
+            <tr>
+              <td><input type="submit" value="Kirim"></input></td>
+            </tr>
+          </table>
+        </form>
+      </div>
+    );
+  }
+
+  export default Buah
